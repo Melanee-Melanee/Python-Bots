a7136aa97f2ce606ae8a45249099572cbf52e42b
Melanee <74653444+Melanee-Melanee@users.noreply.github.com>
2023-05-23 18:30:12 +0330

    Update Melanee robot.py

--- Telegram Bots/Automation/Melanee robot.py
+++ Telegram Bots/Automation/Melanee robot.py
@@ -7,8 +7,6 @@ text= ''' سلام من ربات مِلانی ام. اگه پست های این 
 #https://core.telegram.org/bots/api
 # LINK_2_FORMAT => https://api.telegram.org/bot<ENTER_BOT_TOKEN>/sendMessage?chat_id=<ENTER_CHAT_ID>&text="ENTER YOUR TEXT"
 
-# LINK_2_FORMAT => https://api.telegram.org/bot<ENTER_BOT_TOKEN>/sendMessage?chat_id=<ENTER_CHAT_ID>&text="ENTER YOUR TEXT"
-
 def bot():
     requests.get(f""" https://api.telegram.org/bot<ENTER_BOT_TOKEN>/sendMessage?chat_id=<ENTER_CHAT_ID>&text={text}"""")
 
