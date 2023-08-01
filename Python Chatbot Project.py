444ce32df6952db4839d6c21688ff2729e92bac2
Melanee <74653444+Melanee-Melanee@users.noreply.github.com>
2023-08-01 19:21:43 +0330

    Update Python Chatbot Project.py

--- Python Chatbot Project.py
+++ Python Chatbot Project.py
@@ -14,6 +14,7 @@ import pickle
 
 import numpy as np
 from keras.models import Sequential
+#creating neural networks 
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 import random
